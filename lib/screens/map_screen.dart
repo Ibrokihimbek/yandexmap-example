df2893575e069a051303c4012a62dae41ec7a43a
Ibrohimbek Davronbekov <108320639+Ibrokihimbek@users.noreply.github.com>
2023-04-19 10:06:10 +0500

    some changes

---
+++ lib/screens/map_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class YandexAppPage extends StatefulWidget {
+  const YandexAppPage({Key? key}) : super(key: key);
+
+  @override
+  State<YandexAppPage> createState() => _YandexAppPageState();
+}
+
+class _YandexAppPageState extends State<YandexAppPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
