80a798f3e2a7a2fa60e764abc8cde367cf3a11d0
Ibrohimbek Davronbekov <108320639+Ibrokihimbek@users.noreply.github.com>
2023-04-20 07:55:05 +0500

    some changes

--- android/app/src/main/kotlin/com/example/yandex_mapexample/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/yandex_mapexample/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.yandex_mapexample
-
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import com.yandex.mapkit.MapKitFactory
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        MapKitFactory.setLocale("uz_Cyrl_UZ");
+        MapKitFactory.setApiKey("1634dca9-bc30-4b33-a13c-cf7b8d9e9865")
+        super.configureFlutterEngine(flutterEngine)
+    }
 }
--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:yandex_mapkit/yandex_mapkit.dart';
 
 class YandexAppPage extends StatefulWidget {
   const YandexAppPage({Key? key}) : super(key: key);
@@ -8,8 +10,63 @@ class YandexAppPage extends StatefulWidget {
 }
 
 class _YandexAppPageState extends State<YandexAppPage> {
+  late YandexMapController controller;
+  final animation =
+      const MapAnimation(type: MapAnimationType.smooth, duration: 2.0);
+  Future<bool> get locationPermissionNotGranted async => !(await Permission.location.request().isGranted);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: YandexMap(
+        onMapCreated: (YandexMapController yandexMapController) async {
+          controller = yandexMapController;
+        },
+        onUserLocationAdded: (UserLocationView view) async {
+          return view.copyWith(
+              pin: view.pin.copyWith(
+                  icon: PlacemarkIcon.single(PlacemarkIconStyle(
+                      image: BitmapDescriptor.fromAssetImage(
+                          'assets/user.jpg')))),
+              arrow: view.arrow.copyWith(
+                  icon: PlacemarkIcon.single(PlacemarkIconStyle(
+                      image: BitmapDescriptor.fromAssetImage(
+                          'assets/arrow.png')))),
+              accuracyCircle: view.accuracyCircle
+                  .copyWith(fillColor: Colors.green.withOpacity(0.5)));
+        },
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () async {
+              await controller.moveCamera(CameraUpdate.zoomIn(),
+                  animation: animation);
+            },
+            child: Icon(Icons.add),
+          ),
+          SizedBox(height: 10),
+          FloatingActionButton(
+            onPressed: () async {
+              await controller.moveCamera(CameraUpdate.zoomOut(),
+                  animation: animation);
+            },
+            child: Icon(Icons.remove),
+          ),
+          SizedBox(height: 10),
+          FloatingActionButton(
+            onPressed: () async {
+              if (await locationPermissionNotGranted) {
+                print('Location permission was NOT granted');
+                return;
+              }
+              controller.getUserCameraPosition();
+            },
+            child: Icon(Icons.my_location),
+          ),
+        ],
+      ),
+    );
   }
 }
